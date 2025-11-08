9fc8a26405b95f2744578c74bff5758e6ec89c2d
laymard <aymard.laurent@gmail.com>
2025-11-08 14:49:31 -0500

    feat : add jump

--- GameData/Character/player.gd
+++ GameData/Character/player.gd
@@ -5,6 +5,9 @@ extends CharacterBody3D
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
 
+@export_category("Jump")
+@export var jump_impulse = 20
+
 @export_category("Camera")
 @export var camera_sensitivity = 0.05
 
@@ -60,6 +63,10 @@ func _physics_process(delta: float) -> void:
 	# Vertical Velocity
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y = target_velocity.y - (75*delta)
+	
+		# Jumping.
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
 
 	# Moving the Character
 	velocity = target_velocity
