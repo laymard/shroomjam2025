aa20e43af76795f9a859b5c21a029ca6029337da
laymard <aymard.laurent@gmail.com>
2025-11-09 18:08:36 -0500

    remove vhs count from UI

--- GameData/Level/GMDisplayInventory.gd
+++ GameData/Level/GMDisplayInventory.gd
@@ -5,15 +5,10 @@ extends Label
 @onready
 var Player:GMPlayer = %Player
 
-var _inventoryFormat:String = "TV count : %s \nVHS count : %s"
-
-func _ready() -> void:
-	
-	pass # Replace with function body.
-
+var _inventoryFormat:String = "TV count : %s"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	var actualstring = _inventoryFormat % [Player.Inventory.get_tv_count(), Player.Inventory.get_vhs_count()]
+func _process(_delta: float) -> void:
+	var actualstring = _inventoryFormat % [Player.Inventory.get_tv_count()]
 	text = String(actualstring)
 	pass
